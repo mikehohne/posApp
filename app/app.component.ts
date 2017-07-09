76042d12661fa7c32258263f0227465294b4d7e1
Mike Hohne <mikehohne@gmail.com>
2017-07-09 13:09:14 -0600

    angular setup is completed

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'pm-app',
+  template: `
+  <h1>{{pageTitle}}</h1>
+  `
+})
+
+export class AppComponent {
+    pageTitle: string = 'Hello World';
+}
